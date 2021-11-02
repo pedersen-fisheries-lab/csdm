21826947e29ad319bf11d060f6a198b16b9805e0
Valentin <valentin.lucet@gmail.com>
2021-11-02 10:19:58 -0400

    fix get functions

--- R/get_functions.R
+++ R/get_functions.R
@@ -2,12 +2,12 @@
 # Get functions -----------------------------------------------------------
 
 #' @export
-get_cpue_fit <- function(stan_fit){
+get_cpue_fit <- function(csdm_fit){
 
-  cpue_fit <- csdm_delury %>%
+  cpue_fit <- csdm_fit$fit %>%
     tidybayes::spread_draws(cpue_fit[t]) %>%
     tidybayes::median_qi(.) %>%
-    dplyr::bind_cols(delury) %>%
+    dplyr::bind_cols(csdm_fit$data) %>%
     dplyr::select(-.data$t)
 
   return(cpue_fit)
@@ -15,13 +15,14 @@ get_cpue_fit <- function(stan_fit){
 }
 
 #' @export
-get_biomass <- function(stan_fit){
+get_biomass <- function(csdm_fit){
 
-  biomass <- csdm_delury %>%
-    spread_draws(biomass_ts[t]) %>%
-    median_qi(.) %>%
-    dplur::bind_cols(csdm::delury) %>%
-    select(-.data$t)
+  biomass <- csdm_fit$fit %>%
+    tidybayes::spread_draws(biomass_ts[t]) %>%
+    tidybayes::median_qi(.) %>%
+    dplyr::bind_cols(csdm_fit$data) %>%
+    dplyr::select(-.data$t) %>%
+    dplyr::rename(biomass = .data$biomass_ts)
 
   return(biomass)
 
