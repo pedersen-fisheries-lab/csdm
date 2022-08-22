921ca0e3529d25bcedfc3266a4b8a78c4910d871
Valentin <valentin.lucet@gmail.com>
2022-08-22 10:56:01 -0400

    changes to src

--- src/stanExports_nonlinear_cpue_grouped.cc
+++ src/stanExports_nonlinear_cpue_grouped.cc
@@ -7,7 +7,7 @@ using namespace Rcpp ;
 RCPP_MODULE(stan_fit4nonlinear_cpue_grouped_mod) {
 
 
-    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("model_nonlinear_cpue_grouped")
+    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("rstantools_model_nonlinear_cpue_grouped")
 
     .constructor<SEXP,SEXP,SEXP>()
 
