4c775d3de1945c0db6102ea83f145718cb6b52b4
Valentin <valentin.lucet@gmail.com>
2022-08-22 10:56:13 -0400

    fix plot

--- R/plot.R
+++ R/plot.R
@@ -6,8 +6,8 @@ plot.csdm_fit <- function(x, y, ...){
 
   the_plot <- get_cpue_fit(delury_model) %>%
     ggplot2::ggplot() +
-    ggplot2::geom_point(ggplot2::aes(x=t, y=cpue)) +
-    ggplot2::geom_line(ggplot2::aes(x=t, y=cpue_fit),
+    ggplot2::geom_point(ggplot2::aes(x=.data$t, y=.data$cpue)) +
+    ggplot2::geom_line(ggplot2::aes(x=.data$t, y=.data$cpue_fit),
                        col = "red") +
     ggplot2::labs(x = "Time", y = "CPUE")
 
@@ -20,7 +20,7 @@ plot_biomass <- function(x){
   checkmate::assert_class(x, "csdm_fit")
 
   the_plot <- get_biomass(delury_model) %>%
-    ggplot2::ggplot(ggplot2::aes(x=date, y=biomass)) +
+    ggplot2::ggplot(ggplot2::aes(x=.data$date, y=.data$biomass)) +
     ggplot2::geom_point() +
     ggplot2::geom_line() +
     ggplot2::labs(x = "Time", y = "Biomass")
