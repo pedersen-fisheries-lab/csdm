0bee9b146497231559e063089d959f360e7754a5
Valentin <valentin.lucet@gmail.com>
2022-08-23 12:37:42 -0400

    rename class

---
+++ R/csdm_fit_class.R
@@ -0,0 +1,33 @@
+
+# Imports -----------------------------------------------------------------
+
+#' @importFrom rlang .data
+NULL
+
+# CSDM class object -------------------------------------------------------
+
+#' Create a csdm_fit object
+#'
+#' Creates a csdm_fit object.
+#'
+#' @param data **\[data.frame\]** The model data.
+#' @param fit **\[stanfit\]** The model fit.
+#'
+#' @return
+#' An object of S3 class csdm_fit.
+#'
+#' @examples
+#' \dontrun{
+#' csdm_fit <- new_csdm_fit(data = data, fit = model_fit)
+#' }
+#'
+#' @export
+new_csdm_fit <- function(data, fit){
+
+  stopifnot(is.data.frame(data))
+  stopifnot(class(fit) == "stanfit")
+
+  structure(list(data = data,
+                 fit = fit), class = "csdm_fit")
+
+}
