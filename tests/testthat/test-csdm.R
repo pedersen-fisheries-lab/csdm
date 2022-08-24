42bdb42a96bb2336cdd60c83593ff0f575d7b7ea
Valentin <valentin.lucet@gmail.com>
2022-08-24 11:41:47 -0400

    add tests

---
+++ tests/testthat/test-csdm.R
@@ -0,0 +1,14 @@
+test_that("package fit csdm model correctly", {
+  fit <- csdm(catch = cpue,
+              effort = traps,
+              biomass = ~1,
+              q = ~1,
+              s = ~1,
+              data = delury,
+              prior = list(B0_logmean_prior_sd = log(1000),
+                           B0_group_prior_logsd = log(100)),
+              upper =  c(B0 = 1e10, q = 1e-3, s = 100),
+              chains = 4,
+              iter = 1000)
+  checkmate::expect_class(fit, "csdm_fit")
+})
